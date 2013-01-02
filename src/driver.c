736c79fa2b6d7a197a3446f88881c71ac7eccc87
Ted Ross <ross@office-XPS-8500.(none)>
2013-01-02 07:23:42 -0500

    Increased the buffer size to the MTU, improved the address format.

--- src/driver.c
+++ src/driver.c
@@ -11,6 +11,8 @@
 #include <malloc.h>
 #include <time.h>
 
+#define MTU 1500
+
 int tap_open(char *dev);
 int tun_open(char *dev);
 
@@ -28,14 +30,20 @@ typedef struct ip_header_t {
 } ip_header_t;
 
 
-static uint32_t get_dest_addr(const char *buffer)
+static void get_dest_addr(const char *buffer, char *addr, int len, const char *prefix)
 {
   const ip_header_t *hdr = (const ip_header_t*) buffer;
 
-  if ((hdr->ver_hlen & 0xf0) == 0x40)
-    return ntohl(hdr->dst_addr);
-
-  return 0;
+  if ((hdr->ver_hlen & 0xf0) == 0x40) {
+    uint32_t ip4_addr = ntohl(hdr->dst_addr);
+    snprintf(addr, len, "%s.%d.%d.%d.%d", prefix,
+	     (ip4_addr & 0xFF000000) >> 24,
+	     (ip4_addr & 0x00FF0000) >> 16,
+	     (ip4_addr & 0x0000FF00) >> 8,
+	     (ip4_addr & 0x000000FF));
+  } else {
+    addr[0] = '\0';
+  }
 }
 
 
@@ -45,24 +53,18 @@ int main()
     strcpy(dev, "tun0");
     int fd = tun_open(dev);
     int i;
-    char buffer[1000];
-    char addr_str[16];
+    char buffer[MTU];
+    char addr_str[200];
 
     printf("Tunnel: fd=%d dev=%s\n", fd, dev);
 
     while (1) {
-        ssize_t  len = read(fd, buffer, 1000);
-	uint32_t addr;
+        ssize_t  len = read(fd, buffer, MTU);
 
 	if (len < 20)
 	  continue;
 
-	addr = get_dest_addr(buffer);
-        sprintf(addr_str, "%d.%d.%d.%d", 
-                (unsigned int) ((addr & 0xFF000000) >> 24),
-                (unsigned int) ((addr & 0x00FF0000) >> 16),
-                (unsigned int) ((addr & 0x0000FF00) >> 8),
-                (unsigned int)  (addr & 0x000000FF));
+	get_dest_addr(buffer, addr_str, 200, "vlan");
 
         printf("[ RX to %s (%ld)", addr_str, len);
         for (i = 0; i < len; i++) {
