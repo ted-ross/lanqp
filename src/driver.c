e1d35b71c4a9f4e3c11c0b86bd14a446b1b2f7ed
Ted Ross <ross@office-XPS-8500.(none)>
2013-01-01 20:15:16 -0500

    Use layer-3 tunnel instead of layer-2 tap.

--- src/driver.c
+++ src/driver.c
@@ -1,8 +1,10 @@
+#include <stdint.h>
 #include <sys/socket.h>
 #include <fcntl.h>
 #include <linux/ioctl.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,30 +12,59 @@
 #include <time.h>
 
 int tap_open(char *dev);
+int tun_open(char *dev);
+
+typedef struct ip_header_t {
+  uint8_t  ver_hlen;
+  uint8_t  tos;
+  uint16_t len;
+  uint16_t id;
+  uint16_t flags_offset;
+  uint8_t  ttl;
+  uint8_t  protocol;
+  uint16_t hcksum;
+  uint32_t src_addr;
+  uint32_t dst_addr;
+} ip_header_t;
+
+
+static uint32_t get_dest_addr(const char *buffer)
+{
+  const ip_header_t *hdr = (const ip_header_t*) buffer;
+
+  if ((hdr->ver_hlen & 0xf0) == 0x40)
+    return ntohl(hdr->dst_addr);
+
+  return 0;
+}
+
 
 int main()
 {
     char *dev = malloc(10);
-    strcpy(dev, "tap0");
-    int fd = tap_open(dev);
+    strcpy(dev, "tun0");
+    int fd = tun_open(dev);
     int i;
     char buffer[1000];
-    char addr[16];
+    char addr_str[16];
 
     printf("Tunnel: fd=%d dev=%s\n", fd, dev);
 
     while (1) {
-        ssize_t len = read(fd, buffer, 1000);
+        ssize_t  len = read(fd, buffer, 1000);
+	uint32_t addr;
 
-        sprintf(addr, "%02X%02X%02X%02X%02X%02X",
-                (unsigned char) buffer[0],
-                (unsigned char) buffer[1],
-                (unsigned char) buffer[2],
-                (unsigned char) buffer[3],
-                (unsigned char) buffer[4],
-                (unsigned char) buffer[5]);
+	if (len < 20)
+	  continue;
 
-        printf("[ RX to %s (%ld)", addr, len);
+	addr = get_dest_addr(buffer);
+        sprintf(addr_str, "%d.%d.%d.%d", 
+                (unsigned int) ((addr & 0xFF000000) >> 24),
+                (unsigned int) ((addr & 0x00FF0000) >> 16),
+                (unsigned int) ((addr & 0x0000FF00) >> 8),
+                (unsigned int)  (addr & 0x000000FF));
+
+        printf("[ RX to %s (%ld)", addr_str, len);
         for (i = 0; i < len; i++) {
             printf(" %02x", (unsigned char) buffer[i]);
         }
